777cf2ed82ea9c85231dbbb495c6a853ae416b13
temadps <56594022+temadps@users.noreply.github.com>
2019-10-15 20:24:08 +0700

    matuhalohihi

---
+++ js/1.js
@@ -0,0 +1,11 @@
+ alert ("hello vse kto skachal i kolyan");
+var img='<img src="https://proza.ru/pics/2009/09/06/268.jpg" ></img>';
+
+function f1(){
+    alert("мать коляна шлюшандра");
+}
+
+function f2 (){
+    foto.innerHTML=img
+    
+}
